15b0de6d461c9501507cdb25a2a5ae51f393056d
Varuna Eeriyaulla <lex.varuna@gmail.com>
2018-07-02 10:28:05 +0530

    SubmitField update

--- src/SubmitField.js
+++ src/SubmitField.js
@@ -1,17 +1,21 @@
-import BaseField      from 'uniforms/BaseField';
-import React          from 'react';
+import BaseField from 'uniforms/BaseField';
+import React from 'react';
+import { Button, ButtonIcon } from 'rmwc/Button';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
-const SubmitField = ({disabled, inputRef, value, ...props}, {uniforms: {error, state}}) =>
-    <input
-        disabled={disabled === undefined ? !!(error || state.disabled) : disabled}
-        ref={inputRef}
-        type="submit"
-        {...(value ? {value} : {})}
-        {...filterDOMProps(props)}
-    />
-;
-
+const SubmitField = ({
+  disabled, inputRef, value, buttonIcon, ...props
+}, { uniforms: { error, state } }) =>
+  (
+    <Button
+      disabled={disabled === undefined ? !!(error || state.disabled) : disabled}
+      ref={inputRef}
+      type="submit"
+      {...filterDOMProps(props)}
+    >
+      {buttonIcon ? <ButtonIcon use={buttonIcon} /> : ''}
+      {value || 'Button'}
+    </Button>);
 SubmitField.contextTypes = BaseField.contextTypes;
 
 export default SubmitField;
