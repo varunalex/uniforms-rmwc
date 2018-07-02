46c3b58161f295d8f12a108682109743b9343443
Varuna Eeriyaulla <lex.varuna@gmail.com>
2018-07-02 14:43:20 +0530

    Nested and radio button updates

--- src/NestField.js
+++ src/NestField.js
@@ -1,34 +1,31 @@
-import React          from 'react';
-import connectField   from 'uniforms/connectField';
+import React from 'react';
+import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
-import injectName     from 'uniforms/injectName';
-import joinName       from 'uniforms/joinName';
+import injectName from 'uniforms/injectName';
+import joinName from 'uniforms/joinName';
 
 import AutoField from './AutoField';
 
 const Nest = ({
-    children,
-    fields,
-    itemProps,
-    label,
-    name,
-    ...props
+  children,
+  fields,
+  itemProps,
+  label,
+  name,
+  ...props
 }) =>
-    <div {...filterDOMProps(props)}>
-        {label && (
-            <label>
-                {label}
-            </label>
+  (<div {...filterDOMProps(props)}>
+    {label && (
+    <label>
+      {label}
+    </label>
         )}
 
-        {children ? (
+    {children ? (
             injectName(name, children)
         ) : (
             fields.map(key =>
-                <AutoField key={key} name={joinName(name, key)} {...itemProps} />
-            )
+              <AutoField key={key} name={joinName(name, key)} {...itemProps} />)
         )}
-    </div>
-;
-
-export default connectField(Nest, {ensureValue: false, includeInChain: false});
+   </div>);
+export default connectField(Nest, { ensureValue: false, includeInChain: false });
--- src/RadioField.js
+++ src/RadioField.js
@@ -1,43 +1,39 @@
-import React          from 'react';
-import connectField   from 'uniforms/connectField';
+import React from 'react';
+import { Radio as RadioRMWC } from 'rmwc/Radio';
+import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
 const Radio = ({
-    allowedValues,
-    checkboxes, // eslint-disable-line no-unused-vars
-    disabled,
-    id,
-    label,
-    name,
-    onChange,
-    transform,
-    value,
-    ...props
+  allowedValues,
+  checkboxes, // eslint-disable-line no-unused-vars
+  disabled,
+  id,
+  label,
+  name,
+  onChange,
+  transform,
+  value,
+  ...props
 }) =>
-    <div {...filterDOMProps(props)}>
-        {label && (
-            <label>
-                {label}
-            </label>
+  (<div {...filterDOMProps(props)}>
+    {label && (
+    <label>
+      {label}
+    </label>
         )}
 
-        {allowedValues.map(item =>
-            <div key={item}>
-                <input
-                    checked={item === value}
-                    disabled={disabled}
-                    id={`${id}-${item}`}
-                    name={name}
-                    onChange={() => onChange(item)}
-                    type="radio"
-                />
-
-                <label htmlFor={`${id}-${item}`}>
-                    {transform ? transform(item) : item}
-                </label>
-            </div>
-        )}
-    </div>
-;
-
+    {allowedValues.map(item =>
+          (
+            <RadioRMWC
+              key={item}
+              checked={item === value}
+              disabled={disabled}
+              id={`${id}-${item}`}
+              name={name}
+              onChange={() => onChange(item)}
+              type="radio"
+            >{transform ? transform(item) : item}
+            </RadioRMWC>
+           ))}
+   </div>);
 export default connectField(Radio);
