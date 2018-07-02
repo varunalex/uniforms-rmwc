911b1c79c4b0f0bf49797c57980da9c1ee59da7c
Varuna Eeriyaulla <lex.varuna@gmail.com>
2018-07-02 11:01:57 +0530

    BoolField Update

--- src/BoolField.js
+++ src/BoolField.js
@@ -1,34 +1,36 @@
-import React          from 'react';
-import connectField   from 'uniforms/connectField';
+import React from 'react';
+import { Checkbox } from 'rmwc/Checkbox';
+import connectField from 'uniforms/connectField';
 import filterDOMProps from 'uniforms/filterDOMProps';
 
 const Bool = ({
-    disabled,
-    id,
-    inputRef,
-    label,
-    name,
-    onChange,
-    value,
-    ...props
+  disabled,
+  id,
+  inputRef,
+  indeterminate,
+  label,
+  name,
+  onChange,
+  value,
+  ...props
 }) =>
+  (
     <div {...filterDOMProps(props)}>
-        <input
-            checked={value}
-            disabled={disabled}
-            id={id}
-            name={name}
-            onChange={() => disabled || onChange(!value)}
-            ref={inputRef}
-            type="checkbox"
-        />
-
+      <Checkbox
+        checked={value}
+        disabled={disabled}
+        id={id}
+        indeterminate={indeterminate}
+        name={name}
+        onChange={() => disabled || onChange(!value)}
+        ref={inputRef}
+        type="checkbox"
+      >
         {label && (
-            <label htmlFor={id}>
-                {label}
-            </label>
-        )}
-    </div>
-;
-
+        <label htmlFor={id}>
+          {label}
+        </label>
+            )}
+      </Checkbox>
+    </div>);
 export default connectField(Bool);
