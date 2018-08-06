0dd13b33322ec9515a5140acb72dc8c86d8d76e8
Varuna Eeriyaulla <lex.varuna@gmail.com>
2018-08-06 10:54:36 +0530

    update

--- src/TextField.js
+++ src/TextField.js
@@ -52,7 +52,7 @@ const Text = ({
         {...filterDOMProps(props)}
       />
       {!error ? (
-        <TextFieldHelperText persistent styles="margin: -20px; padding: -20px">
+        <TextFieldHelperText>
           {helpertext}
         </TextFieldHelperText>
       ) : (
